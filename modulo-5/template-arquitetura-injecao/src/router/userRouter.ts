f30fe4b71fd6175439b944832dba2616ed73cb55
lucasbsampaio <lucasaraujosampaio@gmail.com>
2022-08-10 21:24:29 -0300

    meu commit

---
+++ modulo-5/template-arquitetura-injecao/src/router/userRouter.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import { UserBusiness } from '../business/UserBusiness'
+import { UserController } from '../controller/UserController'
+import { UserDatabase } from '../database/UserDatabase'
+import { Authenticator } from '../services/Authenticator'
+import { HashManager } from '../services/HashManager'
+import { IdGenerator } from '../services/IdGenerator'
+
+class UserDatabasePostgres {}
+class UserDatabasePostgres {}
+
+export const userRouter = Router()
+
+const userController = new UserController(new UserBusiness(
+      new UserDatabasePostgres(),
+      new IdGenerator(),
+      new HashManager(),
+      new Authenticator()
+    )
+)
+userRouter.post("/signup", userController.signup)
+userRouter.post("/login", userController.login)
+userRouter.get("/", userController.getUsers)
+userRouter.delete("/:id", userController.deleteUser)
+userRouter.put("/:id", userController.editUser)
